50b68eadaebf78114b654549bbb5de054b5d05d6
Frank Kloeker <eumel@admin.gnuu.de>
2015-12-26 19:25:32 +0100

    just another deploy script

--- deploy.php
+++ deploy.php
@@ -1,50 +1,56 @@
 <?php
-	/**
-	 * GIT DEPLOYMENT SCRIPT
-	 *
-	 * Used for automatically deploying websites via github or bitbucket, more deets here:
-	 *
-	 *		https://gist.github.com/1809044
-	 */
 
-	// The commands
-	$commands = array(
-		'echo $PWD',
-		'whoami',
-		'git pull',
-		'git status',
-		'git submodule sync',
-		'git submodule update',
-		'git submodule status',
-	);
+// Set Variables
+$LOCAL_ROOT         = "/data/www/paste.eumel.de/";
+$LOCAL_REPO_NAME    = "paste2.eumel.de";
+$LOCAL_REPO         = "{$LOCAL_ROOT}/{$LOCAL_REPO_NAME}";
+$REMOTE_REPO        = "git@github.com:eumel8/ZeroBin.git";
+$BRANCH             = "master";
 
-	// Run the commands for output
-	$output = '';
-	foreach($commands AS $command){
-		// Run it
-		$tmp = shell_exec($command);
-		// Output
-		$output .= "<span style=\"color: #6BE234;\">\$</span> <span style=\"color: #729FCF;\">{$command}\n</span>";
-		$output .= htmlentities(trim($tmp)) . "\n";
-	}
+$payload_github = file_get_contents('php://input');
+$data = json_decode($payload_github);
 
-	// Make it pretty for manual user access (and why not?)
-?>
-<!DOCTYPE HTML>
-<html lang="en-US">
-<head>
-	<meta charset="UTF-8">
-	<title>GIT DEPLOYMENT SCRIPT</title>
-</head>
-<body style="background-color: #000000; color: #FFFFFF; font-weight: bold; padding: 0 10px;">
-<pre>
- .  ____  .    ____________________________
- |/      \|   |                            |
-[| <span style="color: #FF0000;">&hearts;    &hearts;</span> |]  | Git Deployment Script v0.1 |
- |___==___|  /              &copy; oodavid 2012 |
-              |____________________________|
+if ( $data->ref === 'refs/heads/master' ) {
 
-<?php echo $output; ?>
-</pre>
-</body>
-</html>
+  // Only respond to POST requests from Github
+  echo "Payload received from GitHub".PHP_EOL;
+
+  if( file_exists($LOCAL_REPO) ) 
+  {
+
+    $whoami = shell_exec("whoami");
+    echo "whoami: $whoami".PHP_EOL;
+
+    // If there is already a repo, just run a git pull to grab the latest changes       
+    $git_pull = shell_exec("git pull 2>&1");
+    echo "Git Pull: $git_pull".PHP_EOL;
+
+    // $composer_install = shell_exec("cd $LOCAL_REPO && composer install 2>&1");
+    // echo "Composer Install: $composer_install".PHP_EOL;
+
+    $artisan_dump = shell_exec("cd $LOCAL_REPO && php artisan dump-autoload 2>&1");
+    echo "PHP Artisan Dump-Autoload: $artisan_dump".PHP_EOL;
+
+    $artisan_migrate = shell_exec("cd $LOCAL_REPO && php artisan migrate --env=production 2>&1");
+    echo "PHP Artisan Migrate: $artisan_migrate".PHP_EOL;
+
+    die("The End! " . mktime());    
+  } 
+  else 
+  {
+
+    // If the repo does not exist, then clone it into the parent directory
+
+    shell_exec("cd {$LOCAL_ROOT} && git clone {$REMOTE_REPO} {$LOCAL_REPO_NAME}");
+    echo "git clone: repo cloned successfully!".PHP_EOL;
+
+    shell_exec("cd {$LOCAL_REPO} && composer install");
+    echo "Executed: composer install".PHP_EOL;
+
+    die("The End! " . mktime());
+  }
+} 
+
+else {
+    echo "Payload is not from GitHub. Nothing to see here!".PHP_EOL;
+}
